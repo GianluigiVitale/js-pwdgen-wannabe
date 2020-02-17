3dfec180109a750b9f6fb0be13a362bc93452a8f
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-17 15:50:58 +0100

    primo esercizio opzionale

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 var nome = prompt("Qual'è il tuo nome?")
 var cognome = prompt('Qual\'è il tuo cognome?')
 var colorePreferito = prompt('Qual\'è il tuo colore preferito?')
-document.getElementById('info-utente').innerHTML = nome + cognome + colorePreferito + 20;
+document.getElementById('info-utente1').innerHTML = nome + cognome + colorePreferito + 20;
+
+var data = new Date();
+var annoCorrente = data.getFullYear();
+document.getElementById('info-utente2').innerHTML = nome + cognome + colorePreferito + annoCorrente;
